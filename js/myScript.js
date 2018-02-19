d83d356dc0aef81ff915abaec011e74d6f5bee64
zaxbatch <34843086+zaxbatch@users.noreply.github.com>
2018-02-19 18:24:07 -0500

    Add files via upload

---
+++ js/myScript.js
@@ -0,0 +1,3 @@
+function myFunction() {
+   document.getElementById("demo").innerHTML = "This fuction was in an External JS File";
+}
---
+++ myScript.js
@@ -0,0 +1,3 @@
+function myFunction2() {
+   document.getElementById("demo06").innerHTML = "Change";
+}
